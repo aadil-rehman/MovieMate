69d55ab9fa630f3ed45b0bf32819bb02be65834b
Moh Aadil <aadilrehman.development@gmail.com>
2024-01-19 22:50:48 +0530

    change bg and api url

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ function Logo() {
 	return (
 		<div className="logo">
 			<span role="img">🍿</span>
-			<h1>usePopcorn</h1>
+			<h1>MovieMate</h1>
 		</div>
 	);
 }
@@ -229,7 +229,7 @@ function MovieDetails({
 			async function getMovieDetails() {
 				setIsLoading(true);
 				const res = await fetch(
-					`http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+					`https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
 				);
 				const data = await res.json();
 
--- src/useMovies.js
+++ src/useMovies.js
@@ -15,7 +15,7 @@ export function useMovies(query) {
 					setIsLoading(true);
 					setError("");
 					const res = await fetch(
-						`http://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`,
+						`https://www.omdbapi.com/?i=tt3896198&apikey=${KEY}&s=${query}`,
 						{ signal: controller.signal }
 					);
 
